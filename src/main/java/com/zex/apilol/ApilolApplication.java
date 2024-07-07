bc517c652418e32b425f301dec66b8ccca4a2e49
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-07 12:39:01 -0300

    raw: uploading the project

---
+++ src/main/java/com/zex/apilol/ApilolApplication.java
@@ -0,0 +1,13 @@
+package com.zex.apilol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zex/apilol/ApilolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zex.apilol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
