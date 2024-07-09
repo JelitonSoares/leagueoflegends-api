18bf4158058ca1d2e1332c9b7d0e09e74f724348
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-08 21:19:38 -0300

    raw: 'creating and mapping entity Skill'

---
+++ src/main/java/com/zex/apilol/models/skill/Skill.java
@@ -0,0 +1,46 @@
+package com.zex.apilol.models.skill;
+
+import com.zex.apilol.models.champion.Champion;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity(name = "skill")
+@Table(name = "skills")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+
+    @Column(name = "skill_name")
+    private String name;
+
+
+    @Column(name = "skill_description")
+    private String description;
+
+
+    @Column(name = "skill_type")
+    @Enumerated(value = EnumType.STRING)
+    private SkillType type;
+
+
+    @Column(name = "skill_image")
+    private String image;
+
+
+    @Column(name = "skill_video")
+    private String video;
+
+    @ManyToOne
+    @JoinColumn(name = "champion_id")
+    private Champion champion;
+
+}
