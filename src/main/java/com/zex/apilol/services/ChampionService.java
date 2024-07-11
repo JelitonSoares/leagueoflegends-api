544bf682936e08a9728c15a21c2bf7d187ca1353
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-10 21:03:45 -0300

    feat: 'creating a ChampionService and implementing a save method to persist one Champion'

---
+++ src/main/java/com/zex/apilol/services/ChampionService.java
@@ -0,0 +1,30 @@
+package com.zex.apilol.services;
+
+import com.zex.apilol.dtos.ChampionDTO;
+import com.zex.apilol.models.champion.Champion;
+import com.zex.apilol.models.skill.Skill;
+import com.zex.apilol.repositories.ChampionRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class ChampionService {
+    @Autowired
+    private ChampionRepository repository;
+
+    @Transactional
+    public Champion save(ChampionDTO data) {
+        Set<Skill> skills = data.skills().stream()
+                .map(Skill::new)
+                .collect(Collectors.toSet());
+
+        Champion champion = new Champion(data);
+        champion.setSkills(skills);
+
+        return this.repository.save(champion);
+    }
+}
