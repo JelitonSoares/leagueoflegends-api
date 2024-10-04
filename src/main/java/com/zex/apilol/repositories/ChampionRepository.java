3ed5a09698ac780baa1cb6c731f6f16f7d86aaa1
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-10-04 16:43:36 -0300

    feat: 'creates method to order all champions by short name'

--- src/main/java/com/zex/apilol/repositories/ChampionRepository.java
+++ src/main/java/com/zex/apilol/repositories/ChampionRepository.java
@@ -2,8 +2,12 @@ package com.zex.apilol.repositories;
 
 import com.zex.apilol.domain.champion.Champion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ChampionRepository extends JpaRepository<Champion, UUID> {
+    @Query("SELECT c FROM Champion c ORDER BY c.shortName")
+    List<Champion> findAllOrderByShortName();
 }
